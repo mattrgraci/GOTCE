f8f6fe7b50c6b4e70fbd9e84fd51186d76d6abe4
mattrgraci <mattgraci@gmail.com>
2025-08-17 16:21:13 -0400

    Delete GOTCE/EntityStatesCustom directory

--- GOTCE/EntityStatesCustom/SolusScanner/BeamEnemy.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace GOTCE.EntityStatesCustom.SolusScanner {
-    public class BeamEnemy : BeamBase {
-        public override float damageCoefficient => 1f;
-        public override TargetType targetType => TargetType.Enemy;
-    }
-}
--- GOTCE/EntityStatesCustom/SolusScanner/BeamFriendly.cs
+++
@@ -1,7 +0,0 @@
-using System;
-
-namespace GOTCE.EntityStatesCustom.SolusScanner {
-    public class BeamFriendly : BeamBase {
-        public override TargetType targetType => TargetType.Friendly;
-    }
-}
